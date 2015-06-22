2b0f8d5f558b878c1088eaaf0a45904576d1fdf0
Trinh <trinhlbk1991@gmail.com>
2015-06-22 23:06:40 +0700

    implemented basic interaction for controls

--- DemoBasiciOSControls/DemoBasiciOSControls/ViewController.swift
+++ DemoBasiciOSControls/DemoBasiciOSControls/ViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var containerLogin: UIView!
     @IBOutlet weak var containerSurvey: UIView!
     @IBOutlet weak var tfName: UITextField!
     @IBOutlet weak var tfPhone: UITextField!
+    @IBOutlet weak var lbSliderValue: UILabel!
+    @IBOutlet weak var sliderSurvey: UISlider!
     
     
     @IBAction func onSegmentValueChanged(sender: UISegmentedControl) {
@@ -33,9 +35,40 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func onBtnLoginClicked(sender: UIButton) {
-        
+    @IBAction func onTextFieldDoneEdit(sender: UITextField) {
+        sender.resignFirstResponder()
     }
-
+    
+    @IBAction func onSliderValueChanged(sender: UISlider) {
+        let sliderValue = sender.value
+        lbSliderValue.text = "\(sliderValue)"
+    }
+    
+    @IBAction func onSwitchSurveyValueChanged(sender: UISwitch) {
+        sliderSurvey.enabled = sender.on
+    }
+    
+    
+    @IBAction func onBtnLoginClicked(sender: UIButton) {
+        let controllerLoginDialog = UIAlertController(title: "Login", message: "Do you want to log in?", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let actionYes = UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler: { action in
+            
+            let controllerLoginSuccessDialog = UIAlertController(title: "Login", message: "Logged in successfully!", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let actionOk = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+            controllerLoginSuccessDialog.addAction(actionOk)
+            self.presentViewController(controllerLoginSuccessDialog, animated: true, completion: nil)
+        })
+        
+        let actionNo = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Destructive, handler: nil)
+        
+        controllerLoginDialog.addAction(actionYes)
+        controllerLoginDialog.addAction(actionNo)
+        presentViewController(controllerLoginDialog, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
